192c88cee43742db06ebde0cfaaaf9a8f088f42b
vũ <vanvux99@gmail.com>
2021-03-15 17:37:16 +0700

    update ngay 15 thang 3

---
+++ pages/cv/templates/_id.vue
@@ -0,0 +1,14 @@
+<template>
+  <AddTemplate />
+</template>
+
+<script>
+import AddTemplate from "../../../components/cv/CreateEditTemplate.vue";
+
+export default {
+  name: "Id",
+  components: { AddTemplate }
+};
+</script>
+
+<style scoped></style>
---
+++ pages/cv/templates/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <ListTemplate />
+</template>
+
+<script>
+import ListTemplate from "../../../components/cv/ListTemplate";
+export default {
+  name: "Index",
+  components: { ListTemplate }
+};
+</script>
+
+<style scoped></style>
